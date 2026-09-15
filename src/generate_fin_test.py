f913398ef34ccd103315d7ea5cee17c5b3cf4c3d
Drew Tyre <atyre2@unl.edu>
2026-09-15 09:01:11 -0500

    fix: adjust fin length to exact hypotenuse and add 0.1mm gap test

--- src/generate_fin_test.py
+++ src/generate_fin_test.py
@@ -9,65 +9,68 @@ def create_fin_test():
     lowest_z = -15 * math.sqrt(2)
     cube = cube.translate((0, 0, -lowest_z))
     
-    # 2. Create the 0.8mm thick wedge that fills the space under the cube.
-    # To avoid zero-thickness non-manifold errors during union, we extend the top
-    # of the fin 0.1mm into the cube.
-    wedge = (
+    # 2. Calculate the exact length on the XY plane for a 30mm hypotenuse
+    L = 30.0 / math.sqrt(2) # 21.2132 mm
+    
+    # 3. Create the two fins. 
+    # Right fin: overlaps by 0.1mm normal (0.141mm vertical) to merge with the cube.
+    wedge_right = (
         cq.Workplane("YZ")
-        .polyline([(0, 0.1), (30, 30.1), (30, 0), (-30, 0), (-30, 30.1)])
+        .polyline([(0,0), (0, 0.141), (L - 0.141, L), (L - 0.141, 0)])
         .close()
         .extrude(0.8)
         .translate((-0.4, 0, 0))
     )
     
-    # 3. Create cutting bodies for the tapers
-    # The taper reduces the width from 0.8 to 0.4 over the last 2mm vertically.
-    # Since we shifted the top up by 0.1mm, we shift the cutting profiles up by 0.1mm (Local Y).
-    prof_rp = [(0.5, 0.2), (0.2, 0.2), (0.5, -1.9)]
-    prof_lp = [(-0.5, 0.2), (-0.2, 0.2), (-0.5, -1.9)]
+    # Left fin: pulled back by 0.1mm normal (0.141mm vertical) to act as a breakaway support.
+    wedge_left = (
+        cq.Workplane("YZ")
+        .polyline([(-0.141, 0), (-L, L - 0.141), (-L, 0)])
+        .close()
+        .extrude(0.8)
+        .translate((-0.4, 0, 0))
+    )
     
-    # Right taper, Y > 0
+    # 4. Create cutting bodies for the tapers
+    prof_p = [(0.5, 0.1), (0.2, 0.1), (0.5, -2.0)]
+    prof_n = [(-0.5, 0.1), (-0.2, 0.1), (-0.5, -2.0)]
+    
+    # Tapers for Right Fin (Shifted UP by 0.141 in Z)
     cut_rp = (
-        cq.Workplane("XZ")
-        .polyline(prof_rp).close()
-        .transformed(offset=(0, 30, 30))
-        .polyline(prof_rp).close()
+        cq.Workplane("XZ", origin=(0, 0, 0.141))
+        .polyline(prof_p).close()
+        .transformed(offset=(0, 25, 25))
+        .polyline(prof_p).close()
         .loft()
     )
-    
-    # Left taper, Y > 0
-    cut_lp = (
-        cq.Workplane("XZ")
-        .polyline(prof_lp).close()
-        .transformed(offset=(0, 30, 30))
-        .polyline(prof_lp).close()
-        .loft()
-    )
-    
-    # Right taper, Y < 0
-    # transformed offset: Local Y=30 -> Global Z=30. Local Z=-30 -> Global Y=-30.
     cut_rn = (
-        cq.Workplane("XZ")
-        .polyline(prof_rp).close()
-        .transformed(offset=(0, 30, -30))
-        .polyline(prof_rp).close()
+        cq.Workplane("XZ", origin=(0, 0, 0.141))
+        .polyline(prof_n).close()
+        .transformed(offset=(0, 25, 25))
+        .polyline(prof_n).close()
         .loft()
     )
+    fin_right = wedge_right.cut(cut_rp).cut(cut_rn)
     
-    # Left taper, Y < 0
+    # Tapers for Left Fin (Shifted DOWN by 0.141 in Z)
+    cut_lp = (
+        cq.Workplane("XZ", origin=(0, 0, -0.141))
+        .polyline(prof_p).close()
+        .transformed(offset=(0, 25, -25))
+        .polyline(prof_p).close()
+        .loft()
+    )
     cut_ln = (
-        cq.Workplane("XZ")
-        .polyline(prof_lp).close()
-        .transformed(offset=(0, 30, -30))
-        .polyline(prof_lp).close()
+        cq.Workplane("XZ", origin=(0, 0, -0.141))
+        .polyline(prof_n).close()
+        .transformed(offset=(0, 25, -25))
+        .polyline(prof_n).close()
         .loft()
     )
+    fin_left = wedge_left.cut(cut_lp).cut(cut_ln)
     
-    # Cut tapers out of the wedge
-    fin = wedge.cut(cut_rp).cut(cut_lp).cut(cut_rn).cut(cut_ln)
-    
-    # 4. Combine the cube and the fin
-    test_obj = cube.union(fin)
+    # 5. Combine everything into a single compound (handles disjoint shells safely)
+    test_obj = cq.Compound.makeCompound([cube.val(), fin_right.val(), fin_left.val()])
     
     return test_obj
 
