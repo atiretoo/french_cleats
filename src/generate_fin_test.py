2a436a02611a732eb0a5ac0fc7c06315e1ccd09d
Drew Tyre <atyre2@unl.edu>
2026-09-15 09:16:09 -0500

    feat: add 3 fin types (overlap, exact, gap) to test model

--- src/generate_fin_test.py
+++ src/generate_fin_test.py
@@ -9,68 +9,98 @@ def create_fin_test():
     lowest_z = -15 * math.sqrt(2)
     cube = cube.translate((0, 0, -lowest_z))
     
-    # 2. Calculate the exact length on the XY plane for a 30mm hypotenuse
-    L = 30.0 / math.sqrt(2) # 21.2132 mm
+def create_fin_half(normal_gap, is_right=True):
+    Z_gap = normal_gap * math.sqrt(2)
+    L = 30.0 / math.sqrt(2)
     
-    # 3. Create the two fins. 
-    # Right fin: overlaps by 0.1mm normal (0.141mm vertical) to merge with the cube.
-    wedge_right = (
+    start_y = max(0, Z_gap)
+    end_y = L + min(0, Z_gap)
+    
+    start_z = start_y - Z_gap
+    end_z = end_y - Z_gap
+    
+    if is_right:
+        pts = [(start_y, 0), (start_y, start_z), (end_y, end_z), (end_y, 0)]
+    else:
+        pts = [(-start_y, 0), (-start_y, start_z), (-end_y, end_z), (-end_y, 0)]
+        
+    unique_pts = [pts[0]]
+    for p in pts[1:]:
+        if p != unique_pts[-1]:
+            unique_pts.append(p)
+    if len(unique_pts) > 1 and unique_pts[-1] == unique_pts[0]:
+        unique_pts.pop()
+        
+    wedge = (
         cq.Workplane("YZ")
-        .polyline([(0,0), (0, 0.141), (L - 0.141, L), (L - 0.141, 0)])
-        .close()
+        .polyline(unique_pts).close()
         .extrude(0.8)
         .translate((-0.4, 0, 0))
     )
     
-    # Left fin: pulled back by 0.1mm normal (0.141mm vertical) to act as a breakaway support.
-    wedge_left = (
-        cq.Workplane("YZ")
-        .polyline([(-0.141, 0), (-L, L - 0.141), (-L, 0)])
-        .close()
-        .extrude(0.8)
-        .translate((-0.4, 0, 0))
-    )
-    
-    # 4. Create cutting bodies for the tapers
     prof_p = [(0.5, 0.1), (0.2, 0.1), (0.5, -2.0)]
     prof_n = [(-0.5, 0.1), (-0.2, 0.1), (-0.5, -2.0)]
     
-    # Tapers for Right Fin (Shifted UP by 0.141 in Z)
-    cut_rp = (
-        cq.Workplane("XZ", origin=(0, 0, 0.141))
-        .polyline(prof_p).close()
-        .transformed(offset=(0, 25, 25))
-        .polyline(prof_p).close()
-        .loft()
-    )
-    cut_rn = (
-        cq.Workplane("XZ", origin=(0, 0, 0.141))
-        .polyline(prof_n).close()
-        .transformed(offset=(0, 25, 25))
-        .polyline(prof_n).close()
-        .loft()
-    )
-    fin_right = wedge_right.cut(cut_rp).cut(cut_rn)
+    if is_right:
+        cut_p = (
+            cq.Workplane("XZ", origin=(0, 0, -Z_gap))
+            .polyline(prof_p).close()
+            .transformed(offset=(0, 30, 30))
+            .polyline(prof_p).close()
+            .loft()
+        )
+        cut_n = (
+            cq.Workplane("XZ", origin=(0, 0, -Z_gap))
+            .polyline(prof_n).close()
+            .transformed(offset=(0, 30, 30))
+            .polyline(prof_n).close()
+            .loft()
+        )
+    else:
+        cut_p = (
+            cq.Workplane("XZ", origin=(0, 0, -Z_gap))
+            .polyline(prof_p).close()
+            .transformed(offset=(0, 30, -30))
+            .polyline(prof_p).close()
+            .loft()
+        )
+        cut_n = (
+            cq.Workplane("XZ", origin=(0, 0, -Z_gap))
+            .polyline(prof_n).close()
+            .transformed(offset=(0, 30, -30))
+            .polyline(prof_n).close()
+            .loft()
+        )
+        
+    return wedge.cut(cut_p).cut(cut_n)
+
+def make_full_fin(normal_gap):
+    return [
+        create_fin_half(normal_gap, is_right=True).val(),
+        create_fin_half(normal_gap, is_right=False).val()
+    ]
+
+def create_fin_test():
+    cube = cq.Workplane("XY").box(30, 30, 30).rotate((0,0,0), (1,0,0), 45)
+    lowest_z = -15 * math.sqrt(2)
+    cube = cube.translate((0, 0, -lowest_z))
     
-    # Tapers for Left Fin (Shifted DOWN by 0.141 in Z)
-    cut_lp = (
-        cq.Workplane("XZ", origin=(0, 0, -0.141))
-        .polyline(prof_p).close()
-        .transformed(offset=(0, 25, -25))
-        .polyline(prof_p).close()
-        .loft()
-    )
-    cut_ln = (
-        cq.Workplane("XZ", origin=(0, 0, -0.141))
-        .polyline(prof_n).close()
-        .transformed(offset=(0, 25, -25))
-        .polyline(prof_n).close()
-        .loft()
-    )
-    fin_left = wedge_left.cut(cut_lp).cut(cut_ln)
+    fins = []
     
-    # 5. Combine everything into a single compound (handles disjoint shells safely)
-    test_obj = cq.Compound.makeCompound([cube.val(), fin_right.val(), fin_left.val()])
+    # Fin 1: Overlap (-0.1mm gap), at X = 10
+    for f in make_full_fin(-0.1):
+        fins.append(f.translate((10, 0, 0)))
+        
+    # Fin 2: Exact (0.0mm gap), at X = 0
+    for f in make_full_fin(0.0):
+        fins.append(f.translate((0, 0, 0)))
+        
+    # Fin 3: Breakaway Gap (0.1mm gap), at X = -10
+    for f in make_full_fin(0.1):
+        fins.append(f.translate((-10, 0, 0)))
+        
+    test_obj = cq.Compound.makeCompound([cube.val()] + fins)
+    return test_obj
     
     return test_obj
 
