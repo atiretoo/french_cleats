54ed28118d8e56f822db2f81049ba6d06b38c5ca
Drew Tyre <atyre2@unl.edu>
2026-09-17 21:20:00 -0500

    Parameterize magnetic holder to support custom magnet sizes and counts

--- src/tool_holders/generate_magnetic_holder.py
+++ src/tool_holders/generate_magnetic_holder.py
@@ -5,7 +5,7 @@ import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from holder_base import create_baseplate, export_stl
 
-def create_magnetic_holder(units=1, mount_type="groove"):
+def create_magnetic_holder(units=1, mount_type="groove", mag_count=2, mag_dia=10.2, mag_depth=2.0):
     unit_width = 28.0
     width = units * unit_width
     
@@ -13,29 +13,35 @@ def create_magnetic_holder(units=1, mount_type="groove"):
     body, top_y, bottom_y, bottom_groove_y, screw_pts, mc_solids = create_baseplate(units=units, mount_type=mount_type, num_rows=2)
     
 
-    # Magnets
-    # 10.2mm diameter, 2.0mm depth.
-    # Center vertically between top_y (20) and bottom_y (-94). Center is -37.0.
-    mag_y1 = -17.0
-    mag_y2 = -57.0
+    # Calculate magnet positions
+    mag_pts = []
+    if mag_count == 1:
+        mag_pts.append((0, -37.0))
+    elif mag_count > 1:
+        start_y = -17.0
+        end_y = -57.0
+        step = (end_y - start_y) / (mag_count - 1)
+        for i in range(mag_count):
+            mag_pts.append((0, start_y + i * step))
     
-    mags = (
-        cq.Workplane("XY").workplane(offset=-11.0) # Front face of baseplate
-        .pushPoints([(0, mag_y1), (0, mag_y2)])
-        .circle(10.2 / 2.0)
-        .extrude(2.0) # Cut into the block (+Z)
-    )
-    body = body.cut(mags)
-    
-    # Push holes
-    # 2mm diameter, all the way through to the back
-    push_holes = (
-        cq.Workplane("XY").workplane(offset=-11.0)
-        .pushPoints([(0, mag_y1), (0, mag_y2)])
-        .circle(2.0 / 2.0)
-        .extrude(20.0) # All the way to +Z
-    )
-    body = body.cut(push_holes)
+    if mag_count > 0:
+        mags = (
+            cq.Workplane("XY").workplane(offset=-11.0) # Front face of baseplate
+            .pushPoints(mag_pts)
+            .circle(mag_dia / 2.0)
+            .extrude(mag_depth) # Cut into the block (+Z)
+        )
+        body = body.cut(mags)
+        
+        # Push holes
+        # 2mm diameter, all the way through to the back
+        push_holes = (
+            cq.Workplane("XY").workplane(offset=-11.0)
+            .pushPoints(mag_pts)
+            .circle(2.0 / 2.0)
+            .extrude(20.0) # All the way to +Z
+        )
+        body = body.cut(push_holes)
     
     # Cut screws
     if screw_pts:
@@ -62,6 +68,21 @@ def create_magnetic_holder(units=1, mount_type="groove"):
     return body
 
 if __name__ == "__main__":
-    holder = create_magnetic_holder(units=1, mount_type="groove")
-    filename = "magnetic_saw_holder_1u_groove.stl"
+    parser = argparse.ArgumentParser(description="Generate magnetic tool holder.")
+    parser.add_argument("--units", type=int, default=1, help="Number of units wide")
+    parser.add_argument("--mount", type=str, default="groove", choices=["groove", "multiconnect", "hybrid"], help="Mount type")
+    parser.add_argument("--mag-count", type=int, default=2, help="Number of magnets")
+    parser.add_argument("--mag-dia", type=float, default=10.2, help="Diameter of magnet hole (including tolerance)")
+    parser.add_argument("--mag-depth", type=float, default=2.0, help="Depth of magnet hole")
+    args = parser.parse_args()
+
+    holder = create_magnetic_holder(
+        units=args.units, 
+        mount_type=args.mount,
+        mag_count=args.mag_count,
+        mag_dia=args.mag_dia,
+        mag_depth=args.mag_depth
+    )
+    
+    filename = f"magnetic_saw_holder_{args.units}u_M{args.mag_count}xD{args.mag_dia}x{args.mag_depth}_{args.mount}.stl"
     export_stl(holder, filename, category="tool_holders")
