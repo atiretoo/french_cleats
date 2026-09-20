045129eb2608e110634bf7fbf4e00681ae1d6320
Drew Tyre <atyre2@unl.edu>
2026-09-20 10:38:05 -0500

    test: align fin tips with cube corners and test micro gap distances

--- src/strength_testing/generate_fin_test.py
+++ src/strength_testing/generate_fin_test.py
@@ -12,11 +12,12 @@ def create_fin_test():
     L = 30.0 / math.sqrt(2)
     
     # Generate 4 fins with different z_gaps and positions
-    # X offsets: -14.2, -5.0, 5.0, 14.2
-    fin1 = support_fin(z_gap=0.07, is_right=True, length=L, height=L).translate((-14.2, 0, 0))
-    fin2 = support_fin(z_gap=0.05, is_right=True, length=L, height=L).translate((-5.0, 0, 0))
-    fin3 = support_fin(z_gap=0.0, is_right=True, length=L, height=L).translate((5.0, 0, 0))
-    fin4 = support_fin(z_gap=0.0, is_right=True, length=L, height=L).translate((14.2, 0, 0))
+    # Align contact tips with the corners (X = -15.0 and X = 15.0)
+    # Tip width is 0.4, so center of fin should be at -14.8 and 14.8
+    fin1 = support_fin(z_gap=0.05, is_right=True, length=L, height=L).translate((-14.8, 0, 0))
+    fin2 = support_fin(z_gap=0.0325, is_right=True, length=L, height=L).translate((-5.0, 0, 0))
+    fin3 = support_fin(z_gap=0.025, is_right=True, length=L, height=L).translate((5.0, 0, 0))
+    fin4 = support_fin(z_gap=0.0, is_right=True, length=L, height=L).translate((14.8, 0, 0))
         
     test_obj = cq.Compound.makeCompound([cube.val(), fin1.val(), fin2.val(), fin3.val(), fin4.val()])
     return test_obj
